7a82ad0d1c1bbbae3ad984e6017e92e846bff417
blingminho <tkdjun67@naver.com>
2018-02-28 13:39:02 +0900

    과제 - CRUD - DB와 연결시켜서 하기

---
+++ src/jdbc/high_20180228/MemberInfoTest.java
@@ -0,0 +1,23 @@
+package jdbc.high_20180228;
+/*
+ * 회원을 관리하는 프로그램을 작성하시오(DB테이블명 : mymember)
+ * 아래 메뉴의 기능을 모두 구현하시오
+ * (CRUD기능 구현하기)
+ * 
+ * ** 자료 삭제는 회원ID를 입력받아서 삭제한다
+ * ** 자료 수정은 회원ID를 입력받아서 수정할 이름, 전화번호, 주소를 입력받아서 수정함
+ * 
+ * 메뉴예시)
+ * -----------------------------------------------
+ * 1. 자료 입력			->	insert (Create)
+ * 2. 자료 수정			->	update
+ * 
+ * 3. 자료 삭제			->	delete
+ * 4. 전체 자료 출력	->	select (Read)
+ * 0. 작업 끝
+ */
+public class MemberInfoTest {
+	public static void main(String[] args) {
+		
+	}
+}
